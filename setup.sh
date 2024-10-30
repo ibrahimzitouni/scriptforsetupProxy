7bd352f756d09b1d9cc48e042e52e0c695ae97e2
ibrahimzitouni <76236650+ibrahimzitouni@users.noreply.github.com>
2024-10-30 19:26:18 +0100

    Update setup.sh
    FINAL VERSION

--- setup.sh
+++ setup.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Check for root privileges
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
-# Update package list and install dependencies
-echo "Updating package list and installing dependencies..."
 apt update && apt install -y python3-pip shadowsocks-libev curl
 
-# Generate the Shadowsocks configuration file
-echo "Creating Shadowsocks configuration..."
-CONFIG_PATH="/etc/shadowsocks-libev/config.json"
-cat > $CONFIG_PATH <<EOL
+cat > /etc/shadowsocks-libev/config.json <<EOL
 {
     "server": "0.0.0.0",
     "server_port": 8388,
@@ -23,25 +17,15 @@ cat > $CONFIG_PATH <<EOL
 }
 EOL
 
-echo "Configuration created at $CONFIG_PATH"
-
-
-# Download ngrok if not already installed
 NGROK_PATH="/usr/local/bin/ngrok"
 if [ ! -f "$NGROK_PATH" ]; then
     echo "Downloading and installing ngrok..."
     wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
     tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 fi
-
-# Prompt for ngrok auth token
+echo "visit this website : https://dashboard.ngrok.com/get-started/your-authtoken"
 read -p "Enter your ngrok auth token: " NGROK_TOKEN
 $NGROK_PATH authtoken $NGROK_TOKEN
 
-# Start Shadowsocks
-echo "Starting Shadowsocks server..."
-ss-server -c $CONFIG_PATH &
-
-# Start ngrok with Shadowsocks server
-echo "Starting ngrok for port $SERVER_PORT..."
-$NGROK_PATH tcp $SERVER_PORT
+ss-server -c /etc/shadowsocks-libev/config.json &
+ngrok tcp 8388
